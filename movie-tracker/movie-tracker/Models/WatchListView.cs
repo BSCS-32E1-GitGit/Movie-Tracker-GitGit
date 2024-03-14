288449a05d46d79ef4cb46ee57e653db49048edb
Jonas R <75775925+Rizardo-Jonas-BSCS32E1@users.noreply.github.com>
2024-03-14 12:14:17 +0800

    MovieTracker-Cinema Done

---
+++ movie-tracker/movie-tracker/Models/WatchListView.cs
@@ -0,0 +1,8 @@
+﻿namespace movie_tracker.Models
+{
+    public class WatchListView
+    {
+        public List<Movie> Movies { get; set; }
+        public List<Movie> WatchList { get; set; }
+    }
+}
